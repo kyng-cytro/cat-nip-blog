342b31ed49ddd83f6ac86cbc535ebddd80c5e37c
kyng-cytro <johndibashi2001@gmail.com>
2024-01-20 14:24:06 +0100

    chore: name change

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,7 +3,7 @@
     <div class="max-w-5xl mx-auto flex justify-between items-center">
     <NuxtLink to="/" class="flex items-center gap-x-2">
       <Icon name="solar:cat-broken" height="25" width="25"/>
-      <h2 class="font-semibold text-lg tracking-tighter leading-none">CAT NIP</h2>
+      <h2 class="font-semibold text-lg tracking-tighter leading-none">NIP</h2>
     </NuxtLink>
     <NuxtLink target="_blank" to="https://github.com/kyng-cytro/cat-nip-blog" >
       <Icon name="uil:github" height="25" width="25"/>
