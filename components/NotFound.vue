3c2d446fa1127e555ac8548a4697659cc464f7c8
kyng-cytro <johndibashi2001@gmail.com>
2024-01-20 12:51:57 +0100

    chore: fixed prose colors

--- components/NotFound.vue
+++ components/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-[80vh] flex flex-col items-center justify-center gap-2">
+    <div class="h-[80vh] flex flex-col items-center justify-center gap-2 text-mauve">
         <h1 class="font-semibold">Why are you here?</h1>
         <NuxtLink to="/" class="shadow-md text-text text-sm py-1 px-2 border rounded-md hover:bg-mauve hover:text-crust">
             Go Back
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="h-full">
+    <div class="h-full py-2">
         <ContentDoc>
             <template v-slot="{doc}">
-                <article class="prose lg:prose-xl">
-                    <h1>{{ doc.title }}</h1>
+                <article class="prose prose-a:text-sapphire prose-strong:text-sapphire  prose-headings:text-sapphire text-pink lg:prose-xl">        
                     <ContentRenderer :value="doc" />
                 </article>
             </template>            
