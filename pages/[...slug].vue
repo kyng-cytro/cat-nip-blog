408cadf0ea98f740843d5900af102983ac7247cf
kyng-cytro <johndibashi2001@gmail.com>
2024-01-20 14:37:52 +0100

    bug: quick size fix

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,7 +2,7 @@
     <div class="max-w-5xl mx-auto h-full py-2">
         <ContentDoc>
             <template v-slot="{doc}">
-                <article class="prose prose-sm max-w-5xl prose-a:text-lavender prose-strong:text-lavender  prose-headings:text-lavender text-mauve lg:prose-xl">        
+                <article class="prose prose-sm max-w-5xl prose-a:text-lavender prose-strong:text-lavender  prose-headings:text-lavender text-mauve lg:prose-md">        
                     <ContentRenderer :value="doc" />
                 </article>
             </template>            
