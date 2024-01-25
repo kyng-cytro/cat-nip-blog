7a552ce36d390112b26ae63280b8fb3326dd3d29
kyng-cytro <johndibashi2001@gmail.com>
2024-01-25 23:22:21 +0100

    fix: A form field element has neither an id nor a name attribute.

--- components/ThemeSwitcher.vue
+++ components/ThemeSwitcher.vue
@@ -3,7 +3,7 @@ const themes = ['frappe', "macchiato",'mocha']
 const theme = useCookie("color-scheme")
 </script>
 <template>
-    <select v-model="theme" class="tracking-tighter text-sm rounded-lg px-1 focus:outline-none capitalize bg-lavender">
+    <select id="theme-switcher" name="theme-switcher" v-model="theme" class="tracking-tighter text-sm rounded-lg px-1 focus:outline-none capitalize bg-lavender">
         <option :key="theme" :value="theme" v-for="theme in themes">{{ theme }}</option>
     </select>
 </template>
